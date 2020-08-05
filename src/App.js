1f1d1a9cc6ca0dccaff008fecddc5a81f02ae2b3
Fergus McGlynn <fergus.a.mcglynn@york.ac.uk>
2020-08-05 11:50:54 +0100

    Fetch people from jsonplaceholder API and output their names

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [people, setPeople] = useState([]);
+
+    useEffect(() => {
+
+        const fetchData = async () => {
+            const response = await fetch("https://jsonplaceholder.typicode.com/users");
+            const json = await response.json();
+            setPeople(json);
+        };
+
+        fetchData();
+
+    }, []);
+
+    return (
+        <div>
+            <p>Hello!</p>
+            <ul>
+                {people.map(person => {
+                    return <li>{person.name}</li>
+                })}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
