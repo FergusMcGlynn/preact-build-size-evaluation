5332143a1a56e8b5c53e4947f4ae3c3718515a1e
Fergus McGlynn <fergus.a.mcglynn@york.ac.uk>
2020-08-05 15:14:47 +0100

    Bloat app with some dependencies

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import {useEffect, useState} from 'preact/hooks'
 import './App.css';
+import _ from 'lodash';
+import {Auth} from "aws-amplify";
+import {BrowserRouter as Router, Route, Switch, Redirect} from "react-router-dom";
+import Select from "react-select";
+import Popup from "reactjs-popup";
+
 
 function App() {
 
@@ -18,13 +24,41 @@ function App() {
 
     }, []);
 
+    const currentUser = async () => {
+        return Auth.currentAuthenticatedUser();
+    };
+
+    const someRoutes = () => {
+        return (
+            <React.Fragment>
+                <Router basename={process.env.PUBLIC_URL}>
+                    <Switch>
+                        <Route exact path="/" render={() => (<Redirect to={process.env.REACT_APP_LIST_PATH}/>)}/>
+                    </Switch>
+                </Router>
+            </React.Fragment>
+        );
+    };
+
+    const someSelect = () => {
+        return (
+            <Select
+                placeholder="Search for items..."
+            />
+        );
+    };
+
+    const somePopup = () => {
+        return (
+            <Popup>Blah</Popup>
+        );
+    };
+
     return (
         <div>
             <p>Hello!</p>
             <ul>
-                {people.map(person => {
-                    return <li>{person.name}</li>
-                })}
+                {_.map(people, person => <li>{person.name}</li>)}
             </ul>
         </div>
     );
